978bf5f76694c1b6083523972aede34c0170628f
Carlos Revilla <crevilla@calculo-sa.es>
2022-11-14 12:00:25 +0100

    component rating and popular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,7 +9,7 @@ import { HeaderComponent } from './header/header.component';
 import { MovieComponent } from './movie/movie.component';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { FeatureModule } from './feature/feature.module';
 
 @NgModule({
   declarations: [
@@ -24,7 +24,7 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
     AppRoutingModule,
     FormsModule,
     HttpClientModule,
-    NgbModule
+    FeatureModule
   ],
   providers: [],
   bootstrap: [AppComponent]
